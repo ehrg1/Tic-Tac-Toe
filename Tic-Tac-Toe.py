53ebf2d4f78e0de23208904a24a688dc1fd1c44e
ehrg1 <riansaad23@gmail.com>
2025-03-18 17:51:52 +0300

    adding a function to determine which player or computer going to start playing

--- Tic-Tac-Toe.py
+++ Tic-Tac-Toe.py
@@ -6,22 +6,39 @@ Board = [
     [0, 0, 0]
 ]
 
-
 turn = True  # Initialize turn variable
 opponent = True  # Initialize opponent variable
+computerTurn = False  # Initialize computerMove variable
+
+def initializeBoard():
+    return [
+        [0, 0, 0],
+        [0, 0, 0],
+        [0, 0, 0]
+    ]
+
+def firstMove():
+    while True:
+        x = input("do you want to play the first move(y/n): ")
+        x = x.lower()
+        if x == 'y':
+            return True
+        elif x == 'n':
+            return False
+        else:
+            print("Invalid Input")
+
 
 def startingMove():
-    global turn
-    x = input("enter the starting move(X, O): ")
-    x = x.upper()
-    if x == 'X':
-        turn = True
-    elif x == 'O':
-        turn = False
-    else:
-        print("Invalid Input")
-        startingMove()
-
+    while True:
+        x = input("enter the starting move(X, O): ")
+        x = x.upper()
+        if x == 'X':
+            return True
+        elif x == 'O':
+            return False
+        else:
+            print("Invalid Input")
 
 
 def moves():
@@ -33,10 +50,11 @@ def moves():
           7 | 8 | 9
           """)
 
+
 def printBoard(Board):
     print("\n\n-----------------------\n\n")
     for i in range(3):
-        for j in range(3):    
+        for j in range(3):
             if Board[i][j] != 0:
                 play = Board[i][j]
             else:
@@ -52,63 +70,65 @@ def printBoard(Board):
 
 
 
-def computerMove():
-    x = random.randint(1, 9)
-    return x
-
-
-
 def checkWin(Board):
     for i in range(3):
         if Board[i][0] == Board[i][1] == Board[i][2] != 0:
             printBoard(Board)
+            print('\U0001F44F' * 25)
             print("Player", Board[i][0], "Wins")
+            print('\U0001F44F' * 25)
             return True
         if Board[0][i] == Board[1][i] == Board[2][i] != 0:
             printBoard(Board)
+            print('\U0001F44F' * 25)
             print("Player", Board[0][i], "Wins")
+            print('\U0001F44F' * 25)
             return True
     if Board[0][0] == Board[1][1] == Board[2][2] != 0:
         printBoard(Board)
+        print('\U0001F44F'*25)
         print("Player", Board[0][0], "Wins")
+        print('\U0001F44F' * 25)
         return True
     if Board[0][2] == Board[1][1] == Board[2][0] != 0:
         printBoard(Board)
+        print('\U0001F44F'*25)
         print("Player", Board[0][2], "Wins")
+        print('\U0001F44F' * 25)
         return True
     return False
 
+
 def checkMove(x):
     x = str(x)
-    if x.isdigit() == False:
+    if not x.isdigit() :
         return True
-    
+
     x = int(x)
     if x < 1 or x > 9:
         return True
     else:
         return False
-        
+
 
 def playMove():
-    global turn, opponent
-    if not opponent and not turn:
-        x = computerMove()
+    global turn, opponent, computerTurn
+    if not opponent and computerTurn:
+        x = random.randint(1, 9) #generate a random move for computer turn
     else:
-        print("Enter the number where you want to place your move (1-9):")
-        x = input()
+        x = input('Enter the number where you want to place your move (1-9): ')
 
     if checkMove(x):
         print("Invalid Move")
         playMove()
         return
-    
+
     x = int(x)
     row = (x - 1) // 3
     col = (x - 1) % 3
-    
+
     if Board[row][col] != 0:
-        if  opponent or turn:
+        if opponent or turn:
             print("Invalid Move")
         playMove()
         return
@@ -119,39 +139,38 @@ def playMove():
         else:
             Board[row][col] = 'O'
             turn = True
-    
+
 
 def game():
+    global computerTurn
     moves()
     for i in range(9):
         printBoard(Board)
         playMove()
+        computerTurn = not computerTurn
         if checkWin(Board):
             return
-    
+
     printBoard(Board)
+
+    print('\U0001F613' * 25)
     print("It's a tie")
-    
+    print('\U0001F613' * 25)
+
 
 while True:
-    play = input("Do you want to play a game with a friend(1) or a computer(2) Eenter 0 to exit? ")
+    play = input("Do you want to play a game with a friend(1) or a computer(2) enter 0 to exit? ")
     if play == '1':
         opponent = True
-        Board = [
-            [0, 0, 0],
-            [0, 0, 0],
-            [0, 0, 0]
-        ]
-        startingMove()
+        Board = initializeBoard()
+        turn = startingMove()
+        computerTurn = False
         game()
     elif play == '2':
         opponent = False
-        Board = [
-            [0, 0, 0],
-            [0, 0, 0],
-            [0, 0, 0]
-        ]  
-        startingMove()
+        Board = initializeBoard()
+        computerTurn = not firstMove()
+        turn = startingMove()
         game()
     elif play == '0':
         break
